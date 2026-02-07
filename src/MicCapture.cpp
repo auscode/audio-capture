0c2b5d7a66a7592365bba2148949f5853c922022
auscode <harshittext@gmail.com>
2026-02-07 22:56:09 +0530

    minimal updates

--- src/MicCapture.cpp
+++ src/MicCapture.cpp
@@ -3,56 +3,52 @@
 #include "Utils.h"
 #include <iostream>
 
-MicCapture::MicCapture(const std::string& outputFile) 
-    : m_outputFile(outputFile) {
+MicCapture::MicCapture(const std::string &outputFile)
+    : m_outputFile(outputFile)
+{
 }
 
-bool MicCapture::start() {
+bool MicCapture::start()
+{
     Utils::createDirectory("output");
-    
+
 #ifdef PLATFORM_WINDOWS
-    if (!initializeWaveIn()) {
+    if (m_bRunning)
         return false;
-    }
-    startInternal();
-    // Open wave in device for microphone recording
-    MMRESULT result = waveInOpen(&m_hWaveIn, WAVE_MAPPER, &m_waveFormat, 
-                                (DWORD_PTR)waveInProc, (DWORD_PTR)this, 
-                                CALLBACK_FUNCTION);
-    
-    if (result != MMSYSERR_NOERROR) {
-        std::cerr << "Failed to open wave in device: " << result << std::endl;
+
+    if (!initializeWaveIn())
         return false;
-    }
 
     m_bRunning = true;
-    m_hThread = CreateThread(nullptr, 0, audioThreadProc, this, 0, nullptr);
-    
-    return m_hThread != nullptr;
+    startInternal(); // this calls waveInStart()
+
+    return true;
 #else
-    // Linux stub implementation
     m_bRunning = true;
-    m_pThread = new std::thread(audioThreadProc, this);
     return true;
 #endif
 }
 
-void MicCapture::stop() {
+void MicCapture::stop()
+{
     m_bRunning = false;
-    
+
 #ifdef PLATFORM_WINDOWS
-    if (m_hWaveIn) {
+    if (m_hWaveIn)
+    {
         waveInStop(m_hWaveIn);
         waveInReset(m_hWaveIn);
     }
-    
-    if (m_hThread) {
+
+    if (m_hThread)
+    {
         WaitForSingleObject(m_hThread, 5000);
         CloseHandle(m_hThread);
         m_hThread = nullptr;
     }
 #else
-    if (m_pThread) {
+    if (m_pThread)
+    {
         m_pThread->join();
         delete m_pThread;
         m_pThread = nullptr;
@@ -60,9 +56,10 @@ void MicCapture::stop() {
 #endif
 }
 
-void MicCapture::audioThread() {
+{
     WavWriter writer(m_outputFile, 44100, 2, 16);
-    if (!writer.initialize()) {
+    if (!writer.initialize())
+    {
         std::cerr << "Failed to initialize WAV writer" << std::endl;
         return;
     }
@@ -70,49 +67,54 @@ void MicCapture::audioThread() {
 #ifdef PLATFORM_WINDOWS
     // Windows-specific microphone capture implementation
     const int BUFFER_SIZE = 4096;
-    WAVEHDR* waveHeaders = new WAVEHDR[2];
-    BYTE* buffers = new BYTE[BUFFER_SIZE * 2];
-    
+    WAVEHDR *waveHeaders = new WAVEHDR[2];
+    BYTE *buffers = new BYTE[BUFFER_SIZE * 2];
+
     // Prepare wave headers
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++)
+    {
         waveHeaders[i].lpData = (LPSTR)(buffers + i * BUFFER_SIZE);
         waveHeaders[i].dwBufferLength = BUFFER_SIZE;
         waveHeaders[i].dwFlags = 0;
         waveInPrepareHeader(m_hWaveIn, &waveHeaders[i], sizeof(WAVEHDR));
         waveInAddBuffer(m_hWaveIn, &waveHeaders[i], sizeof(WAVEHDR));
     }
-    
+
     // Start recording
     waveInStart(m_hWaveIn);
-    
-    while (m_bRunning) {
+
+    while (m_bRunning)
+    {
         Utils::sleep(10);
     }
-    
+
     // Clean up
     waveInStop(m_hWaveIn);
     waveInReset(m_hWaveIn);
-    
-    for (int i = 0; i < 2; i++) {
+
+    for (int i = 0; i < 2; i++)
+    {
         waveInUnprepareHeader(m_hWaveIn, &waveHeaders[i], sizeof(WAVEHDR));
     }
-    
+
     delete[] waveHeaders;
     delete[] buffers;
-    
+
     // Write some dummy data for demonstration
     const char silence[BUFFER_SIZE] = {0};
-    for (int i = 0; i < 100 && m_bRunning; i++) {
-        writer.write((const uint8_t*)silence, BUFFER_SIZE);
+    for (int i = 0; i < 100 && m_bRunning; i++)
+    {
+        writer.write((const uint8_t *)silence, BUFFER_SIZE);
         Utils::sleep(10);
     }
 #else
     // Linux stub - create a silent WAV file
     const int BUFFER_SIZE = 4096;
     char silence[BUFFER_SIZE] = {0};
-    
-    for (int i = 0; i < 100 && m_bRunning.load(); i++) {
-        writer.write((const uint8_t*)silence, BUFFER_SIZE);
+
+    for (int i = 0; i < 100 && m_bRunning.load(); i++)
+    {
+        writer.write((const uint8_t *)silence, BUFFER_SIZE);
         Utils::sleep(10);
     }
 #endif
